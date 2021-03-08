00c44fe40a2088374d4f9b313d8aafebfc494118
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-03-08 21:16:41 +0100

    Initial Commit

---
+++ BMI Calculator/BMI_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_CalculatorApp.swift
+//  BMI Calculator
+//
+//  Created by sanmi_personal on 08/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct BMI_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMI Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BMI Calculator
+//
+//  Created by sanmi_personal on 08/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
